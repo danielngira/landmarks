37b5158d4949782f64b6128d0ca1ea23d47ce74d
danielngira <danielngira7@gmail.com>
2023-11-09 19:44:46 -0600

    Setting up

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Daniel Ngira on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
